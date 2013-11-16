9500a0d26532e7183e5ff4e8363f41b76e5a2fc2
xuwei-k <6b656e6a69@gmail.com>
2013-11-16 19:14:21 +0900

    0.13.1 SNAPSHOT launcher

--- travis.sh
+++ travis.sh
@@ -1,4 +1,4 @@
-wget http://typesafe.artifactoryonline.com/typesafe/ivy-releases/org.scala-sbt/sbt-launch/0.13.1-RC2/sbt-launch.jar &&
+wget http://typesafe.artifactoryonline.com/typesafe/ivy-snapshots/org.scala-sbt/sbt-launch/0.13.1-20131116-062322/sbt-launch.jar &&
 rm project/build.properties &&
 echo "sbt.version=$TRAVIS_SCALA_VERSION" > project/build.properties &&
 java -XX:MaxPermSize=1G -Xmx2G -jar ./sbt-launch.jar scripted
