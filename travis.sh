2c033e7c24a76937e67a9f557e436d2ef450a6b4
xuwei-k <6b656e6a69@gmail.com>
2013-11-16 18:53:47 +0900

    download from github

--- travis.sh
+++ travis.sh
@@ -1,2 +1,4 @@
-sbt -XX:MaxPermSize=1G -Xmx2G -sbt-version $TRAVIS_SCALA_VERSION scripted
+wget https://raw.github.com/paulp/sbt-extras/1c1f38e34fcdf/sbt &&
+chmod +x sbt &&
+./sbt -XX:MaxPermSize=1G -Xmx2G -sbt-version $TRAVIS_SCALA_VERSION scripted
 
